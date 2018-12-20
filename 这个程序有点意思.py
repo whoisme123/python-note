7848a3c275472d85465554a8ff6ad2523d3f2b11
whoisme123 <40683917+whoisme123@users.noreply.github.com>
2018-12-20 21:18:42 +0800

    Create 这个程序有点意思.py

---
+++ 这个程序有点意思.py
@@ -0,0 +1,14 @@
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return ' 李雪智障'
+    elif answerNumber == 2:
+        return '李雪聪明'
+    elif answerNumber == 3:
+        return '李雪sb'
+    elif answerNumber == 4:
+        return '李雪美丽'
+r = random.randint(1, 4)
+forturn = getAnswer(r)
+print(forturn)
